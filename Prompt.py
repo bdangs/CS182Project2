cc910693f5a4743a817b73ed077be0820040116d
ca8maristela <57410056+ca8maristela@users.noreply.github.com>
2023-12-04 21:51:09 -0800

    Can generate text but still has some bugs

--- Prompt.py
+++ Prompt.py
@@ -1,11 +1,12 @@
 from transformers import GPT2LMHeadModel
 from transformers import GPT2Tokenizer
+import torch
 
-xmlGenerator = GPT2LMHeadModel.from_pretrained("gpt2")
+xmlModel = GPT2LMHeadModel.from_pretrained("gpt2")
 
-tokens = GPT2Tokenizer.from_pretrained("gpt2")
+xmlTokenizer = GPT2Tokenizer.from_pretrained("gpt2")
 
-xmlGenerator.eval()
+xmlModel.eval()
 
 xmlInput = "example.xml"  # input file
 
@@ -14,17 +15,23 @@ with open(xmlInput, "r", encoding="utf-8") as file:  # reads input xml file
 
 # we would put our prompt statements here for gpt2 to use. Need 5. Could probably create a loop to make code easier
 # not sure if this is how the prompt is supposed to be
-prompt1 = "Please generate an xml file based on animals like the xml file provided."
+prompt1 = "Godzilla has surfaced and is destroying tokyo!"
 # prompt2
 # prompt3
 # prompt4
 # prompt5
 
-input = prompt1 + inputFile  # combine prompt wth example
+input = prompt1  # combine prompt wth example
 # seems to be some sort of error here on line 25
-xmlFile = xmlGenerator.generate(tokens.encode(input))
-output = tokens.decode(xmlFile)
+encoded_input = xmlTokenizer.encode(input, return_tensors="pt")
+# xmlFile = xmlModel.generate(tokenizer.encode(input))
+# output = tokenizer.decode(xmlFile)
+output = xmlModel.generate(encoded_input, max_length=50,
+                           temperature=1.0, top_k=45, pad_token_id=xmlTokenizer.eos_token_id)
+
+xmlText = xmlTokenizer.decode(output[0], skip_special_tokens=True)
+# print(xmlText)
 
 outputFile = "xmlFile1.xml"  # for first prompt
 with open(outputFile, "w") as file:
-    file.write(output)
+    file.write(xmlText)
